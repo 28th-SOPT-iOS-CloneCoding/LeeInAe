153781b1f9b13cdae77928eec21f5731fb26174a
inaeLee <inae9709@hanmail.net>
2021-04-28 00:44:10 +0900

    add: cell textView 터치 시 키보드 보이기

--- reminder-iOS-clone/reminder-iOS-clone/Sources/Cells/Detail/DetailGroupCell.swift
+++ reminder-iOS-clone/reminder-iOS-clone/Sources/Cells/Detail/DetailGroupCell.swift
@@ -7,13 +7,19 @@
 
 import UIKit
 
+protocol DetailGroupCellDelegate {
+    func updateHeightRow(_ cell: DetailGroupCell, _ textView: UITextView)
+}
+
 class DetailGroupCell: UITableViewCell {
     static let identifier = "DetailGroupCell"
 
     @IBOutlet var circleView: UIView!
     @IBOutlet var radioButton: RadioButton!
+    @IBOutlet var titleTextView: UITextView!
 
     var color: UIColor?
+    var delegate: DetailGroupCellDelegate?
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -21,18 +27,30 @@ class DetailGroupCell: UITableViewCell {
         circleView.layer.borderWidth = 1.4
         circleView.layer.borderColor = .init(gray: 0.6, alpha: 1)
         circleView.layer.cornerRadius = circleView.bounds.width / 2
-        
+
         radioButton.bgColor = .red
         radioButton.isChecked = false
+
+        titleTextView.text = "새로운 미리 알림"
+        titleTextView.isScrollEnabled = false
+        titleTextView.delegate = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
 
-    // FIXME: - color 로컬 변수로 바꾸기
+    // FIXME: - color 로컬 변수로 바꾸기 + Noti로 누름 처리 + table delete
     @IBAction func touchUpRadionBtn(_ sender: Any) {
         print("눌렀슴니다요")
         radioButton.touchUpButton(color: .red, borderSize: 1.4)
+    }
+}
+
+extension DetailGroupCell: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        if let delegate = self.delegate {
+            delegate.updateHeightRow(self, titleTextView)
+        }
     }
 }
--- reminder-iOS-clone/reminder-iOS-clone/Sources/VCs/Detail/DetailGroupVC.swift
+++ reminder-iOS-clone/reminder-iOS-clone/Sources/VCs/Detail/DetailGroupVC.swift
@@ -24,8 +24,6 @@ class DetailGroupVC: UIViewController {
         super.viewDidLoad()
 
         initView()
-        print("#############")
-        print(self.group)
     }
 }
 
@@ -42,7 +40,7 @@ extension DetailGroupVC {
 
         groupTableView.delegate = self
         groupTableView.dataSource = self
-        
+
         // FIXME: - 외않되
         groupTableView.largeContentTitle = group?.title
     }
@@ -50,18 +48,46 @@ extension DetailGroupVC {
 
 // MARK: - UITableViewDelegate
 
-extension DetailGroupVC: UITableViewDelegate {}
+extension DetailGroupVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.resignFirstResponder()
+    }
+}
 
 // MARK: - UITableViewDataSource
 
 extension DetailGroupVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        2
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: DetailGroupCell.identifier) as? DetailGroupCell else { return UITableViewCell() }
+        cell.delegate = self
 
         return cell
+    }
+}
+
+// FIXME: - 동작 안 함..
+
+// MARK: - DetailGroupCellDelegate
+
+extension DetailGroupVC: DetailGroupCellDelegate {
+    func updateHeightRow(_ cell: DetailGroupCell, _ textView: UITextView) {
+        let size = textView.bounds.size
+        let newSize = groupTableView.sizeThatFits(CGSize(width: size.width, height: CGFloat.greatestFiniteMagnitude))
+
+        if size.height != newSize.height {
+            print(size.height, newSize.height)
+            UIView.setAnimationsEnabled(false)
+            groupTableView.beginUpdates()
+            groupTableView.endUpdates()
+            UIView.setAnimationsEnabled(true)
+
+            if let idx = groupTableView.indexPath(for: cell) {
+                groupTableView.scrollToRow(at: idx, at: .bottom, animated: true)
+            }
+        }
     }
 }
