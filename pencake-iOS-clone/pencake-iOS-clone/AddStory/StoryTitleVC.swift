8bc1c745040efe00213fd1cd75097c40a64c027a
inaeLee <inae9709@hanmail.net>
2021-05-27 02:17:34 +0900

    fix: navigationBar shadow 제거, closeButton 추가

--- pencake-iOS-clone/pencake-iOS-clone/AddStory/StoryTitleVC.swift
+++ pencake-iOS-clone/pencake-iOS-clone/AddStory/StoryTitleVC.swift
@@ -8,17 +8,42 @@
 import UIKit
 
 class StoryTitleVC: UIViewController {
+    // MARK: - UIComponents
+
+    private lazy var closeButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(title: "닫기", style: .plain, target: self, action: #selector(touchUpCloseButton(_:)))
+
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setView()
+        setNavigation()
     }
 }
+
+// MARK: - Action Methods
+
+extension StoryTitleVC {
+    @objc func touchUpCloseButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - Custom Methods
 
 extension StoryTitleVC {
     func setView() {
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+    }
+
+    func setNavigation() {
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.backgroundColor = .white
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+
+        self.navigationItem.leftBarButtonItem = self.closeButton
     }
 }
-
-
