c266738850a37d49708c6df88378cbd7c720faf9
inaeLee <inae9709@hanmail.net>
2021-06-03 21:16:11 +0900

    add: subTitleVC 구성 (titleVC와 동일)

--- pencake-iOS-clone/pencake-iOS-clone/AddStory/StorySubTitleVC.swift
+++ pencake-iOS-clone/pencake-iOS-clone/AddStory/StorySubTitleVC.swift
@@ -16,16 +16,55 @@ class StorySubTitleVC: UIViewController {
         return button
     }()
 
+    private lazy var subTitleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "예) 내일도 먹어야지!"
+        textField.font = UIFont.NotoSerifKR(type: .regular, size: 18)
+        textField.textAlignment = .center
+        textField.borderStyle = .none
+
+        return textField
+    }()
+
+    private let guideLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.NotoSerifKR(type: .regular, size: 18)
+        label.text = "이야기의 소제목을 입력해주세요.\n다짐말을 써도 좋아요."
+        label.numberOfLines = 2
+        label.sizeToFit()
+        label.textAlignment = .center
+
+        return label
+    }()
+
     // MARK: - local variables
 
     var storyTitle: String?
+    let labelTopAnchor: CGFloat = -120
 
     // MARK: - LifeCycle Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setView()
         setNavigationBar()
+        setNotification()
+        setConstraint()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        /// textfield border
+        let border = CALayer()
+        border.frame = CGRect(x: 0, y: self.subTitleTextField.frame.size.height - 1, width: self.subTitleTextField.frame.width, height: 1)
+        border.backgroundColor = UIColor.systemGray4.cgColor
+        self.subTitleTextField.layer.addSublayer(border)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
 }
 
@@ -33,16 +72,82 @@ class StorySubTitleVC: UIViewController {
 
 extension StorySubTitleVC {
     @objc func touchUpCompletionButton(_ sender: UIBarButtonItem) {
-        ContainerVC.pages.append(StoryVC(viewModel: StoryViewModel()))
+        guard let title = subTitleTextField.text else { return }
 
-        self.dismiss(animated: true, completion: nil)
+        if title.isEmpty {
+            let alert = UIAlertController(title: nil, message: "소제목을 입력해주세요", preferredStyle: .alert)
+            let submitAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+            alert.addAction(submitAction)
+
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            ContainerVC.pages.append(StoryVC(viewModel: StoryViewModel()))
+
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+
+    @objc func willShowKeyboard(_ noti: Notification) {
+        let topAnchor = self.labelTopAnchor - 40
+
+        self.guideLabel.snp.updateConstraints { make in
+            make.centerY.equalToSuperview().offset(topAnchor)
+        }
+
+        UIView.animate(withDuration: 0.8) {
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @objc func willHideKeyboard(_ noti: Notification) {
+        let topAnchor = self.labelTopAnchor + 40
+
+        self.guideLabel.snp.updateConstraints { make in
+            make.centerY.equalToSuperview().offset(topAnchor)
+        }
+
+        UIView.animate(withDuration: 0.8) {
+            self.view.layoutIfNeeded()
+        }
     }
 }
 
 // MARK: - Custom Methods
 
 extension StorySubTitleVC {
+    func setView() {
+        view.backgroundColor = .white
+
+        self.subTitleTextField.becomeFirstResponder()
+    }
+
     func setNavigationBar() {
-        self.navigationItem.rightBarButtonItem = self.completionButton
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barTintColor = .white
+        navigationController?.navigationBar.shadowImage = UIImage()
+
+        navigationItem.rightBarButtonItem = self.completionButton
+        navigationItem.backBarButtonItem?.tintColor = .lightGray
+        navigationController?.navigationBar.topItem?.title = ""
+    }
+
+    func setConstraint() {
+        view.addSubviews([self.guideLabel, self.subTitleTextField])
+
+        self.guideLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(labelTopAnchor)
+        }
+
+        self.subTitleTextField.snp.makeConstraints { make in
+            make.top.equalTo(guideLabel.snp.bottom).offset(40)
+            make.height.equalTo(guideLabel.bounds.height + 10)
+            make.leading.trailing.equalToSuperview().inset(40)
+        }
+    }
+
+    func setNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.willShowKeyboard(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.willHideKeyboard(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
