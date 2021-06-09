4c12a7870e00f0f3924f847e0d3e2800081fcd3f
inaeLee <inae9709@hanmail.net>
2021-06-10 02:08:39 +0900

    add: AddStoryVC - menuVC 이야기 추가 버튼 isHidden

--- pencake-iOS-clone/pencake-iOS-clone/View/Menu/MenuVC.swift
+++ pencake-iOS-clone/pencake-iOS-clone/View/Menu/MenuVC.swift
@@ -19,6 +19,17 @@ class MenuVC: UIViewController {
         return button
     }()
 
+    private lazy var addStoryButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("이야기 추가", for: .normal)
+        button.titleLabel?.font = UIFont.NotoSerifKR(type: .regular, size: 20)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.isHidden = true
+        button.addTarget(self, action: #selector(touchUpAddStoryButton(_:)), for: .touchUpInside)
+
+        return button
+    }()
+
     private lazy var addWritingButton: UIButton = {
         let button = UIButton()
         button.setTitle("글 추가", for: .normal)
@@ -63,7 +74,7 @@ class MenuVC: UIViewController {
 
 private extension MenuVC {
     func setConstraint() {
-        view.addSubviews([deleteStoryButton, addWritingButton, closeButton])
+        view.addSubviews([deleteStoryButton, addWritingButton, closeButton, addStoryButton])
 
         deleteStoryButton.snp.makeConstraints { make in
             make.top.equalToSuperview().inset(250)
@@ -72,6 +83,11 @@ private extension MenuVC {
 
         addWritingButton.snp.makeConstraints { make in
             make.top.equalTo(deleteStoryButton.snp.bottom).offset(60)
+            make.centerX.equalToSuperview()
+        }
+
+        addStoryButton.snp.makeConstraints { make in
+            make.top.equalTo(addWritingButton.snp.bottom).offset(60)
             make.centerX.equalToSuperview()
         }
 
@@ -84,11 +100,24 @@ private extension MenuVC {
 
     func setView() {
         view.backgroundColor = .white
+
+        if ContainerVC.currPage == 0 {
+            addStoryButton.isHidden = false
+        }
     }
 }
 
 extension MenuVC {
     @objc func touchUpCloseButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+
+    @objc func touchUpAddStoryButton(_ sender: UIButton) {
+        let storyTitleVC = StoryTitleVC()
+        let navigationController = UINavigationController(rootViewController: storyTitleVC)
+
+        navigationController.modalPresentationStyle = .overCurrentContext
+
+        present(navigationController, animated: true, completion: nil)
     }
 }
